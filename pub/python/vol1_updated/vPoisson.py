b32ae4981b7c074f67e948ad40c6f2e3f14f723a
li50@llnl.gov <liruipengblue@gmail.com>
2021-03-15 23:24:17 -0700

    vPoisson update

--- pub/python/vol1_updated/vPoisson.py
+++ pub/python/vol1_updated/vPoisson.py
@@ -9,7 +9,10 @@ Test problem is chosen to give an exact solution at all nodes of the mesh.
 from __future__ import print_function
 from fenics import *
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib import cm
 import numpy
+from numpy import linalg as LA
 from scipy.sparse import csr_matrix
 from scipy.io import mmwrite
 
@@ -31,27 +34,66 @@ def printCoords(V):
     elif(dim == 3):
         numpy.savetxt("mat.coords", coords, header=header, comments='', fmt="%24.15e %24.15e %24.15e")
 
-def vPoisson():
+def surfaceplot(nx, ny, u):
+    V = u.function_space()
+    v2d = vertex_to_dof_map(V)
+    coords = V.tabulate_dof_coordinates()
+    u_array = u.vector().get_local()
+    coords = coords[v2d,:]
+    u_array = u_array[v2d]
+    X = coords[:,0].reshape(nx+1, ny+1)
+    Y = coords[:,1].reshape(nx+1, ny+1)
+    Z = u_array.reshape(nx+1, ny+1)
+    fig = plt.figure()
+    ax = fig.gca(projection='3d')
+    surf = ax.plot_surface(X, Y, Z, cmap=cm.coolwarm,
+                           rstride=1, cstride=1)
+    fig.colorbar(surf)
+
+def symdiff(l,k):
+    from sympy import exp, sin, pi
+    import sympy as sym
+    H = lambda x: exp(-16*(x-0.5)**2)*sin(l*pi*x)
+    x, y = sym.symbols('x[0], x[1]')
+    u = H(x)*H(y)
+    u_code = sym.printing.ccode(u)
+    u_code = u_code.replace('M_PI', 'pi')
+    u_D = Expression(u_code, degree=4)
+    #
+    K = lambda x,y: exp(sin(k*pi*x*y))
+    kappa = K(x,y)
+    f = sym.diff(-kappa*sym.diff(u, x), x) + \
+        sym.diff(-kappa*sym.diff(u, y), y)
+    f = sym.simplify(f)
+    f_code = sym.printing.ccode(f)
+    f_code = f_code.replace('M_PI', 'pi')
+    f = Expression(f_code, degree=1)
+
+    #print('C code for u:', u_code)
+    #print('C code for f:', f_code)
+    #pdb.set_trace()
+
+    return u_D, f
+
+def vPoisson(nx=32,ny=32,debug=1):
     # Create mesh and define function space
-    mesh = UnitSquareMesh(32, 32)
+    mesh = UnitSquareMesh(nx, ny)
     V = FunctionSpace(mesh, 'P', 1)
 
     #pdb.set_trace()
     #printCoords(V)
 
-    # Define boundary condition
-    # u_D = Expression('1 + x[0]*x[0] + 2*x[1]*x[1]', degree=2)
-    # u_D = Constant(1)
-    a = 4*pi
-    b = 4*pi
-    u_D = Expression('sin(a*x[0]-pi/2)*sin(b*x[1]-pi/2)', degree=6, a=a, b=b)
-    kappa = Expression('sin(a*x[0]+b*x[1])', degree=6, a=a, b=b)
-    f = Expression('(a*a+b*b)*sin(a*x[0]-pi/2)*sin(b*x[1]-pi/2)', degree=6, a=a, b=b)
+    l = 3
+    k = 2
+    kappa = Expression('exp(sin(k*pi*x[0]*x[1]))', degree=1, k=k)
+    #kappa = Expression('exp(sin(k*pi*x[0]) * sin(k*pi*x[1]))', degree=6, k=k)
 
-    #plot(kappa, mesh=mesh)
-    c = plot(kappa, mesh=mesh)
-    plt.colorbar(c)
-    plt.show()
+    #plt.figure(2)
+    #c = plot(kappa, mesh=mesh)
+    #plt.colorbar(c)
+
+    # Define boundary condition and rhs
+    u_D, f = symdiff(l,k)
 
     def boundary(x, on_boundary):
         return on_boundary
@@ -61,7 +103,7 @@ def vPoisson():
     # Define variational problem
     u = TrialFunction(V)
     v = TestFunction(V)
-    a = dot(grad(u), grad(v))*dx
+    a = kappa*dot(grad(u), grad(v))*dx
     L = f*v*dx
 
     A, b = assemble_system(a, L, bc)
@@ -73,6 +115,9 @@ def vPoisson():
     # Apply boundary conditions
     #bc.apply(A, b)
 
+    if debug > 0:
+        print('cond(A) =', LA.cond(A.array()))
+
     Acsr = csr_matrix(A.array())
 
     mmwrite('A.mtx',Acsr,symmetry='general')
@@ -83,23 +128,30 @@ def vPoisson():
     solve(a == L, u, bc)
 
     # Plot solution and mesh
-    plot(u)
+    #plt.figure(1)
+    #c = plot(u)
     #plot(mesh)
+    #plt.colorbar(c)
 
-    # Save solution to file in VTK format
-    #vtkfile = File('poisson/solution.pvd')
-    #vtkfile << u
+    u2 = project(u, V)
+    surfaceplot(nx, ny, u2)
 
+    u3 = project(u_D, V)
+    surfaceplot(nx, ny, u3)
+
+    # Compute error in L2 norm
+    error_L2 = errornorm(u_D, u, 'L2')
+    print('error_L2 =', errornorm(u_D, u, 'L2'))
+
+    # Compute error in LA
     A_array = A.array()
     u_array = u.vector().get_local()
     b_array = b.get_local()
     r_array = b_array - A_array @ u_array
-
-    # Print errors
     print('error_max =', numpy.linalg.norm(r_array))
 
     # Hold plot
     plt.show()
 
 if __name__ == '__main__':
-    vPoisson()
+    vPoisson(nx=31,ny=31,debug=0)
